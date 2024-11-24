33aab83e0ca7b9908259e952f2b5471dab9ec4ca
Cort <150208139+probablycort@users.noreply.github.com>
2024-11-24 15:11:20 +0800

    update

--- cogs/defaultcmds.py
+++
@@ -1,8 +0,0 @@
-import discord
-from discord.ext import commands
-from discord import app_commands
-
-class defaultcmds(commands.cog):
-    def __init__(self, client: commands.Bot):
-        self.client = client
-
